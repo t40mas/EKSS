27f5b0144f04e1cb398d1cbd667e407536a836bf
Thomas Fricke <tho.fricke@me.com>
2015-04-15 19:18:56 +0200

    init

--- src/main/java/de/ekss/chat/ejb/TestJSF.java
+++ src/main/java/de/ekss/chat/ejb/TestJSF.java
@@ -1,13 +1,24 @@
 package de.ekss.chat.ejb;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import java.io.Serializable;
 
 /**
  * Created by thomas on 15.04.15.
  */
 
+@Named
 @SessionScoped
 public class TestJSF implements Serializable{
 
+    private String name = "test";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
