bd625e153e4d91eae5e35ce66461229b0c4fa452
Thomas Fricke <tho.fricke@me.com>
2015-04-15 19:01:27 +0200

    init

---
+++ src/main/java/de/ekss/chat/jpa/User.java
@@ -0,0 +1,44 @@
+package de.ekss.chat.jpa;
+
+import javax.persistence.*;
+
+/**
+ * Created by thomas on 15.04.15.
+ */
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique=true, nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private String password;
+
+    public User(){}
+
+    public User(String name, String password){
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
