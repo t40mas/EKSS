8753c74c906e9ee07fca41a4f739a3ad6be1f14c
Thomas Fricke <tho.fricke@me.com>
2015-04-15 19:16:22 +0200

    init

---
+++ src/main/java/de/ekss/chat/ejb/TestJSF.java
@@ -0,0 +1,13 @@
+package de.ekss.chat.ejb;
+
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ * Created by thomas on 15.04.15.
+ */
+
+@SessionScoped
+public class TestJSF implements Serializable{
+
+}
